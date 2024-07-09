39ce1395f0d787fdfc1aef5dd04b409046c24a8d
aaditya1995-dev <aaditya1995@gmail.com>
2024-07-09 16:35:10 +0100

    deleted st.echo

--- tennis.py
+++ tennis.py
@@ -1,11 +1,9 @@
 import streamlit as st
-
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.chrome.options import Options
-    from datetime import datetime, timedelta
-    from collections import defaultdict
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime, timedelta
+from collections import defaultdict
 
 # Function to check availability
 def check_availability(url):
